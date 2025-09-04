88049476b91b7fa955a74198a4ebdbea15638642
NaingMinThant77 <naingminthant07@gmail.com>
2025-09-04 21:09:59 +0630

    Server_setup

---
+++ server/src/app.ts
@@ -0,0 +1,30 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDB } from "./db/dbConnect";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const app = express();
+
+// middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(json());
+app.use(cookieParser());
+
+// routes
+
+const PORT = process.env.PORT || "8000";
+app.listen(PORT, () => {
+  // database connection
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ server/src/db/dbConnect.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    let DB_CONNECTION_STRING = "";
+    if (process.env.NODE_ENV === "development") {
+      DB_CONNECTION_STRING = process.env.MONGODB_LOCAL_URL!;
+    }
+    if (process.env.NODE_ENV === "production") {
+      DB_CONNECTION_STRING = process.env.MONGODB_URL!;
+    }
+
+    const response = await mongoose.connect(DB_CONNECTION_STRING);
+    console.log("DB connected - ", response.connection.host);
+  } catch (error) {
+    console.log("DB connection error - ", error);
+    process.exit(1);
+  }
+};
